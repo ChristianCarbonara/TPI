1585ce70407c43822d17ecf8b77b931e117cb074
Christian Carbonara <christcarbo@hotmail.com>
2018-05-16 16:33:43 +0200

    Version 16.05.2018 - 16:31
    Le journal de travail est à jour.
    Le rapport a besoin d'être avancé (actuellement 12 pages).
    Le programme vient d'être créé.
    
    Problème d'accès pour le programme à vérifier d'urgence.

---
+++ Programme/Convertisseur_de_bases/Convertisseur_de_bases/Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Convertisseur_de_bases
+{
+    partial class fntProgram
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tsMenu = new System.Windows.Forms.ToolStrip();
+            this.SuspendLayout();
+            // 
+            // tsMenu
+            // 
+            this.tsMenu.Location = new System.Drawing.Point(0, 0);
+            this.tsMenu.Name = "tsMenu";
+            this.tsMenu.Size = new System.Drawing.Size(332, 25);
+            this.tsMenu.TabIndex = 0;
+            this.tsMenu.Text = "toolStrip1";
+            // 
+            // fntProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 302);
+            this.Controls.Add(this.tsMenu);
+            this.Name = "fntProgram";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Convertisseur de bases";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip tsMenu;
+    }
+}
+
