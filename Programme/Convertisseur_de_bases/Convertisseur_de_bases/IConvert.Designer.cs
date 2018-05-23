373e795034be5bd9a53bf6aa123a743deecdd0e2
Christian Carbonara <christcarbo@hotmail.com>
2018-05-23 16:33:32 +0200

    Version du 23.05.2018 - 16:31
    Plusieurs crash sont survenu durant le codage du programme.
    
    Le rapport nécessite encore d'être avancé cependant il faut avancer dans le programme.
    
    Le journal de travail est à jour mais il faut ajouter les problèmes de crash de Visual Studio encore.
    
    Un screen du crash de Visual Studio a été fait.
    
    La maquette pour la soustraction avance mais il faut vérifier avec le chef de projet.

--- Programme/Convertisseur_de_bases/Convertisseur_de_bases/IConvert.Designer.cs
+++ Programme/Convertisseur_de_bases/Convertisseur_de_bases/IConvert.Designer.cs
@@ -29,19 +29,22 @@
         private void InitializeComponent()
         {
             this.tsMenu = new System.Windows.Forms.ToolStrip();
+            this.tsddbChoseMode = new System.Windows.Forms.ToolStripDropDownButton();
+            this.tsmiModeAddition = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmiModeSubtract = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsddbOptions = new System.Windows.Forms.ToolStripDropDownButton();
+            this.tsbHelp = new System.Windows.Forms.ToolStripButton();
             this.cobListFormat = new System.Windows.Forms.ComboBox();
-            this.txbValueUserBeforPoint = new System.Windows.Forms.TextBox();
+            this.txbValueUserBeforePoint = new System.Windows.Forms.TextBox();
             this.txbValueUserAfterPoint = new System.Windows.Forms.TextBox();
             this.lblPoint = new System.Windows.Forms.Label();
             this.txbResultConvLeft = new System.Windows.Forms.TextBox();
             this.txbResultConvMiddle = new System.Windows.Forms.TextBox();
             this.txbResultConvRight = new System.Windows.Forms.TextBox();
             this.btnConvert = new System.Windows.Forms.Button();
-            this.tsddbChoseMode = new System.Windows.Forms.ToolStripDropDownButton();
-            this.tsmiModeAddition = new System.Windows.Forms.ToolStripMenuItem();
-            this.tsmiModeSubtract = new System.Windows.Forms.ToolStripMenuItem();
-            this.tsddbOptions = new System.Windows.Forms.ToolStripDropDownButton();
-            this.tsbHelp = new System.Windows.Forms.ToolStripButton();
+            this.lblResultConvertLeft = new System.Windows.Forms.Label();
+            this.lblResultConvertMiddle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.tsMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -53,85 +56,9 @@
             this.tsbHelp});
             this.tsMenu.Location = new System.Drawing.Point(0, 0);
             this.tsMenu.Name = "tsMenu";
-            this.tsMenu.Size = new System.Drawing.Size(449, 25);
+            this.tsMenu.Size = new System.Drawing.Size(701, 25);
             this.tsMenu.TabIndex = 0;
             this.tsMenu.Text = "toolStrip1";
-            // 
-            // cobListFormat
-            // 
-            this.cobListFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
-            this.cobListFormat.Items.AddRange(new object[] {
-            "Décimale",
-            "Binaire",
-            "Octal",
-            "Hexadécimal"});
-            this.cobListFormat.Location = new System.Drawing.Point(158, 91);
-            this.cobListFormat.Name = "cobListFormat";
-            this.cobListFormat.Size = new System.Drawing.Size(121, 21);
-            this.cobListFormat.TabIndex = 2;
-            this.cobListFormat.SelectedIndexChanged += new System.EventHandler(this.cobListFormat_SelectedIndexChanged);
-            // 
-            // txbValueUserBeforPoint
-            // 
-            this.txbValueUserBeforPoint.Location = new System.Drawing.Point(107, 118);
-            this.txbValueUserBeforPoint.Name = "txbValueUserBeforPoint";
-            this.txbValueUserBeforPoint.Size = new System.Drawing.Size(100, 20);
-            this.txbValueUserBeforPoint.TabIndex = 3;
-            this.txbValueUserBeforPoint.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
-            this.txbValueUserBeforPoint.TextChanged += new System.EventHandler(this.txbValueUserBeforPoint_TextChanged);
-            // 
-            // txbValueUserAfterPoint
-            // 
-            this.txbValueUserAfterPoint.Location = new System.Drawing.Point(225, 118);
-            this.txbValueUserAfterPoint.Name = "txbValueUserAfterPoint";
-            this.txbValueUserAfterPoint.Size = new System.Drawing.Size(100, 20);
-            this.txbValueUserAfterPoint.TabIndex = 4;
-            this.txbValueUserAfterPoint.TextChanged += new System.EventHandler(this.txbValueUserAfterPoint_TextChanged);
-            // 
-            // lblPoint
-            // 
-            this.lblPoint.AutoSize = true;
-            this.lblPoint.Location = new System.Drawing.Point(212, 123);
-            this.lblPoint.Name = "lblPoint";
-            this.lblPoint.Size = new System.Drawing.Size(10, 13);
-            this.lblPoint.TabIndex = 5;
-            this.lblPoint.Text = ".";
-            // 
-            // txbResultConvLeft
-            // 
-            this.txbResultConvLeft.Location = new System.Drawing.Point(25, 204);
-            this.txbResultConvLeft.Name = "txbResultConvLeft";
-            this.txbResultConvLeft.ReadOnly = true;
-            this.txbResultConvLeft.Size = new System.Drawing.Size(100, 20);
-            this.txbResultConvLeft.TabIndex = 6;
-            this.txbResultConvLeft.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // txbResultConvMiddle
-            // 
-            this.txbResultConvMiddle.Location = new System.Drawing.Point(168, 204);
-            this.txbResultConvMiddle.Name = "txbResultConvMiddle";
-            this.txbResultConvMiddle.ReadOnly = true;
-            this.txbResultConvMiddle.Size = new System.Drawing.Size(100, 20);
-            this.txbResultConvMiddle.TabIndex = 7;
-            this.txbResultConvMiddle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // txbResultConvRight
-            // 
-            this.txbResultConvRight.Location = new System.Drawing.Point(322, 204);
-            this.txbResultConvRight.Name = "txbResultConvRight";
-            this.txbResultConvRight.ReadOnly = true;
-            this.txbResultConvRight.Size = new System.Drawing.Size(100, 20);
-            this.txbResultConvRight.TabIndex = 8;
-            this.txbResultConvRight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            // 
-            // btnConvert
-            // 
-            this.btnConvert.Location = new System.Drawing.Point(347, 289);
-            this.btnConvert.Name = "btnConvert";
-            this.btnConvert.Size = new System.Drawing.Size(75, 23);
-            this.btnConvert.TabIndex = 9;
-            this.btnConvert.Text = "Convertir";
-            this.btnConvert.UseVisualStyleBackColor = true;
             // 
             // tsddbChoseMode
             // 
@@ -147,13 +74,13 @@
             // tsmiModeAddition
             // 
             this.tsmiModeAddition.Name = "tsmiModeAddition";
-            this.tsmiModeAddition.Size = new System.Drawing.Size(152, 22);
+            this.tsmiModeAddition.Size = new System.Drawing.Size(137, 22);
             this.tsmiModeAddition.Text = "Additionner";
             // 
             // tsmiModeSubtract
             // 
             this.tsmiModeSubtract.Name = "tsmiModeSubtract";
-            this.tsmiModeSubtract.Size = new System.Drawing.Size(152, 22);
+            this.tsmiModeSubtract.Size = new System.Drawing.Size(137, 22);
             this.tsmiModeSubtract.Text = "Soustraire";
             // 
             // tsddbOptions
@@ -173,18 +100,125 @@
             this.tsbHelp.Size = new System.Drawing.Size(23, 22);
             this.tsbHelp.Text = "?";
             // 
+            // cobListFormat
+            // 
+            this.cobListFormat.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cobListFormat.Items.AddRange(new object[] {
+            "Décimale",
+            "Binaire",
+            "Octal",
+            "Hexadécimal"});
+            this.cobListFormat.Location = new System.Drawing.Point(232, 97);
+            this.cobListFormat.Name = "cobListFormat";
+            this.cobListFormat.Size = new System.Drawing.Size(121, 21);
+            this.cobListFormat.TabIndex = 2;
+            this.cobListFormat.SelectedIndexChanged += new System.EventHandler(this.cobListFormat_SelectedIndexChanged);
+            // 
+            // txbValueUserBeforePoint
+            // 
+            this.txbValueUserBeforePoint.Location = new System.Drawing.Point(181, 124);
+            this.txbValueUserBeforePoint.Name = "txbValueUserBeforePoint";
+            this.txbValueUserBeforePoint.Size = new System.Drawing.Size(100, 20);
+            this.txbValueUserBeforePoint.TabIndex = 3;
+            this.txbValueUserBeforePoint.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.txbValueUserBeforePoint.TextChanged += new System.EventHandler(this.txbValueUserBeforePoint_TextChanged);
+            // 
+            // txbValueUserAfterPoint
+            // 
+            this.txbValueUserAfterPoint.Location = new System.Drawing.Point(299, 124);
+            this.txbValueUserAfterPoint.Name = "txbValueUserAfterPoint";
+            this.txbValueUserAfterPoint.Size = new System.Drawing.Size(100, 20);
+            this.txbValueUserAfterPoint.TabIndex = 4;
+            this.txbValueUserAfterPoint.TextChanged += new System.EventHandler(this.txbValueUserAfterPoint_TextChanged);
+            // 
+            // lblPoint
+            // 
+            this.lblPoint.AutoSize = true;
+            this.lblPoint.Location = new System.Drawing.Point(286, 129);
+            this.lblPoint.Name = "lblPoint";
+            this.lblPoint.Size = new System.Drawing.Size(10, 13);
+            this.lblPoint.TabIndex = 5;
+            this.lblPoint.Text = ".";
+            // 
+            // txbResultConvLeft
+            // 
+            this.txbResultConvLeft.Location = new System.Drawing.Point(25, 204);
+            this.txbResultConvLeft.Name = "txbResultConvLeft";
+            this.txbResultConvLeft.ReadOnly = true;
+            this.txbResultConvLeft.Size = new System.Drawing.Size(196, 20);
+            this.txbResultConvLeft.TabIndex = 6;
+            this.txbResultConvLeft.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txbResultConvMiddle
+            // 
+            this.txbResultConvMiddle.Location = new System.Drawing.Point(299, 204);
+            this.txbResultConvMiddle.Name = "txbResultConvMiddle";
+            this.txbResultConvMiddle.ReadOnly = true;
+            this.txbResultConvMiddle.Size = new System.Drawing.Size(100, 20);
+            this.txbResultConvMiddle.TabIndex = 7;
+            this.txbResultConvMiddle.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txbResultConvRight
+            // 
+            this.txbResultConvRight.Location = new System.Drawing.Point(574, 204);
+            this.txbResultConvRight.Name = "txbResultConvRight";
+            this.txbResultConvRight.ReadOnly = true;
+            this.txbResultConvRight.Size = new System.Drawing.Size(100, 20);
+            this.txbResultConvRight.TabIndex = 8;
+            this.txbResultConvRight.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // btnConvert
+            // 
+            this.btnConvert.Location = new System.Drawing.Point(614, 308);
+            this.btnConvert.Name = "btnConvert";
+            this.btnConvert.Size = new System.Drawing.Size(75, 23);
+            this.btnConvert.TabIndex = 9;
+            this.btnConvert.Text = "Convertir";
+            this.btnConvert.UseVisualStyleBackColor = true;
+            this.btnConvert.Click += new System.EventHandler(this.btnConvert_Click);
+            // 
+            // lblResultConvertLeft
+            // 
+            this.lblResultConvertLeft.AutoSize = true;
+            this.lblResultConvertLeft.Location = new System.Drawing.Point(22, 188);
+            this.lblResultConvertLeft.Name = "lblResultConvertLeft";
+            this.lblResultConvertLeft.Size = new System.Drawing.Size(85, 13);
+            this.lblResultConvertLeft.TabIndex = 10;
+            this.lblResultConvertLeft.Text = "Résultat gauche";
+            // 
+            // lblResultConvertMiddle
+            // 
+            this.lblResultConvertMiddle.AutoSize = true;
+            this.lblResultConvertMiddle.Location = new System.Drawing.Point(296, 188);
+            this.lblResultConvertMiddle.Name = "lblResultConvertMiddle";
+            this.lblResultConvertMiddle.Size = new System.Drawing.Size(75, 13);
+            this.lblResultConvertMiddle.TabIndex = 11;
+            this.lblResultConvertMiddle.Text = "Résultat milieu";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(571, 188);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Résultat droite";
+            // 
             // fntProgram
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(449, 333);
+            this.ClientSize = new System.Drawing.Size(701, 343);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblResultConvertMiddle);
+            this.Controls.Add(this.lblResultConvertLeft);
             this.Controls.Add(this.btnConvert);
             this.Controls.Add(this.txbResultConvRight);
             this.Controls.Add(this.txbResultConvMiddle);
             this.Controls.Add(this.txbResultConvLeft);
             this.Controls.Add(this.lblPoint);
             this.Controls.Add(this.txbValueUserAfterPoint);
-            this.Controls.Add(this.txbValueUserBeforPoint);
+            this.Controls.Add(this.txbValueUserBeforePoint);
             this.Controls.Add(this.cobListFormat);
             this.Controls.Add(this.tsMenu);
             this.Name = "fntProgram";
@@ -200,10 +234,9 @@
         #endregion
 
         private System.Windows.Forms.ToolStrip tsMenu;
-        private System.Windows.Forms.TextBox txbValueUserBeforPoint;
+        private System.Windows.Forms.TextBox txbValueUserBeforePoint;
         private System.Windows.Forms.TextBox txbValueUserAfterPoint;
         private System.Windows.Forms.Label lblPoint;
-        private System.Windows.Forms.ComboBox cobListFormat;
         private System.Windows.Forms.TextBox txbResultConvLeft;
         private System.Windows.Forms.TextBox txbResultConvMiddle;
         private System.Windows.Forms.TextBox txbResultConvRight;
@@ -213,6 +246,10 @@
         private System.Windows.Forms.ToolStripMenuItem tsmiModeSubtract;
         private System.Windows.Forms.ToolStripDropDownButton tsddbOptions;
         private System.Windows.Forms.ToolStripButton tsbHelp;
+        private System.Windows.Forms.Label lblResultConvertLeft;
+        private System.Windows.Forms.Label lblResultConvertMiddle;
+        protected internal System.Windows.Forms.ComboBox cobListFormat;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- Programme/Convertisseur_de_bases/Convertisseur_de_bases/IConvert.cs
+++ Programme/Convertisseur_de_bases/Convertisseur_de_bases/IConvert.cs
@@ -11,6 +11,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -24,32 +25,65 @@ namespace Convertisseur_de_bases
     /// </summary>
     public partial class fntProgram : Form
     {
+        // Permet d'utiliser les switchs suivent le format choisi
+        const string BIN_TEXT = "Binaire";
+        const string DEC_TEXT = "Décimale";
+        const string HEX_TEXT = "Héxadécimal";
+        const string OCT_TEXT = "Octal";
+
+        // Valeur max en bits pour chaque format
+        const int NBR_BITS_BIN_MAX = 31;
+        const int NBR_BITS_DEC_MAX = 0;
+        const int NBR_BITS_HEX_MAX = 0;
+        const int NBR_BITS_OCT_MAX = 11;
+
+        // Tableau pour stocker le résultat de chaque format
+        int[] tabConvBin = new int[NBR_BITS_BIN_MAX];
+        int[] tabConvDec = new int[NBR_BITS_DEC_MAX];
+        int[] tabConvHex = new int[NBR_BITS_HEX_MAX];
+        int[] tabConvOct = new int[NBR_BITS_OCT_MAX];
+
+        int valueUserEnter;
+        int nbrBitsUser;
+
+        // Regex pour vérifier si l'utilisateur a entré uniquement des chiffres
+        Regex checkValDec = new Regex("^[0-9]+$");
+
         /// <summary>
         /// 
         /// </summary>
         public fntProgram()
         {
             InitializeComponent();
+            lblResultConvertLeft.Text = "";
+            lblResultConvertMiddle.Text = "";
         }
 
         /// <summary>
-        /// 
+        /// Méthode qui permet à chaque changement de format d'indiquer à l'utilisateur si le nombre entrer 
+        /// correspond au format accépté ou non
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
         private void cobListFormat_SelectedIndexChanged(object sender, EventArgs e)
         {
+            // Permet de vérifier si la valeur que l'utilisateur correspond au format choisi
+            switch(cobListFormat.Text)
+            {
+                case DEC_TEXT:
+                    if (checkValDec.IsMatch(txbValueUserBeforePoint.Text) || txbValueUserBeforePoint.Text == "")
+                    {
+                        txbValueUserBeforePoint.BackColor = Color.White;
+                    }
+                    else
+                    {
+                        txbValueUserBeforePoint.BackColor = Color.MediumVioletRed;
+                    }
 
-        }
-
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void txbValueUserBeforPoint_TextChanged(object sender, EventArgs e)
-        {
-
+                    lblResultConvertLeft.Text = BIN_TEXT;
+                    lblResultConvertMiddle.Text = OCT_TEXT;
+                    break;
+            }
         }
 
         /// <summary>
@@ -60,6 +94,143 @@ namespace Convertisseur_de_bases
         private void txbValueUserAfterPoint_TextChanged(object sender, EventArgs e)
         {
 
+        }
+
+        /// <summary>
+        /// Bouton pour effectuer la conversion du nombre entrer suivent le format
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnConvert_Click(object sender, EventArgs e)
+        {
+            string resultToShowBin = "";
+            string resultToShowOCt = "";
+            int nbrBitsInTabBin = 0;
+            int nbrBitsInTabOct = 0;
+            int nbrBitsBlockShow = 0;
+            int nbrBitsBlockShowMax = 4;
+
+            // Permet de convertir suivent le format que l'utilisateur choisit
+            switch (cobListFormat.Text)
+            {
+                case DEC_TEXT:
+                    if (checkValDec.IsMatch(txbValueUserBeforePoint.Text))
+                    {
+                        valueUserEnter = Convert.ToInt32(txbValueUserBeforePoint.Text);
+                        nbrBitsUser = txbValueUserBeforePoint.Text.Length;
+
+                        // Permet de convertir le nombre en binaire et le stocker dans un tableau
+                        for (int countNbr = 0; countNbr < NBR_BITS_BIN_MAX; countNbr++)
+                        {
+                            tabConvBin[countNbr] = valueUserEnter % 2;
+
+                            if (tabConvBin[countNbr] == 1)
+                            {
+                                valueUserEnter = (valueUserEnter - 1) / 2;
+                            }
+                            else
+                            {
+                                valueUserEnter = valueUserEnter / 2;
+                            }
+
+                            if (valueUserEnter == 0)
+                            {
+                                break;
+                            }
+
+                            nbrBitsInTabBin += 1;
+                        }
+
+                        valueUserEnter = Convert.ToInt32(txbValueUserBeforePoint.Text);
+                        // Permet de convertir le nombre en octal et le stocker dans un tableau
+                        for (int countNbr = 0; countNbr < NBR_BITS_OCT_MAX; countNbr++)
+                        {
+                            tabConvOct[countNbr] = valueUserEnter % 8;
+
+                            if (tabConvOct[countNbr] != 0)
+                            {
+                                valueUserEnter = (valueUserEnter - (valueUserEnter % 8)) / 8;
+                            }
+                            else
+                            {
+                                valueUserEnter = valueUserEnter / 8;
+                            }
+
+                            if (valueUserEnter == 0)
+                            {
+                                break;
+                            }
+
+                            nbrBitsInTabOct += 1;
+                        }
+                        break;
+                    }
+                    else
+                    {
+                        break;
+                    }
+            }
+            
+            // Stock le résultat sur une variable en récupérant dans chaque case du tableau les valeurs
+            for (int countNbrInverse = nbrBitsInTabBin; countNbrInverse >= 0; countNbrInverse--)
+            {
+                // Permet d'espacer les chiffres une fois qu'un certain nombre de chiffres ont été affichés
+                if (nbrBitsBlockShow < nbrBitsBlockShowMax)
+                {
+                    resultToShowBin = resultToShowBin + Convert.ToString(tabConvBin[countNbrInverse]);
+                    nbrBitsBlockShow++;
+                }
+                else
+                {
+                    resultToShowBin = resultToShowBin + " ";
+                    nbrBitsBlockShow = 0;
+                }
+            }
+
+            txbResultConvLeft.Text = resultToShowBin;
+
+            nbrBitsBlockShow = 0;
+            nbrBitsBlockShowMax = 4;
+
+            // Stock le résultat sur une variable en récupérant dans chaque case du tableau les valeurs
+            for (int countNbrInverse = nbrBitsInTabOct; countNbrInverse >= 0; countNbrInverse--)
+            {
+                // Permet d'espacer les chiffres une fois qu'un certain nombre de chiffres ont été affichés
+                if (nbrBitsBlockShow < nbrBitsBlockShowMax)
+                {
+                    resultToShowOCt = resultToShowOCt + Convert.ToString(tabConvOct[countNbrInverse]);
+                    nbrBitsBlockShow++;
+                }
+                else
+                {
+                    resultToShowOCt = resultToShowOCt + " ";
+                    nbrBitsBlockShow = 0;
+                }
+            }
+            txbResultConvMiddle.Text = resultToShowOCt;
+
+        }
+
+        /// <summary>
+        /// Suivent
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txbValueUserBeforePoint_TextChanged(object sender, EventArgs e)
+        {
+            switch (cobListFormat.Text)
+            {
+                case DEC_TEXT:
+                    if (checkValDec.IsMatch(txbValueUserBeforePoint.Text))
+                    {
+                        txbValueUserBeforePoint.BackColor = Color.White;
+                    }
+                    else
+                    {
+                        txbValueUserBeforePoint.BackColor = Color.MediumVioletRed;
+                    }
+                    break;  
+            }
         }
     }
 }
